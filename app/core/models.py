620c38ec7c5864d00fb6de71cd26a9e50e49c1c5
Philip <philip0227@tamu.edu>
2022-06-06 21:56:01 -0500

    apply migration add model pass model test

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,40 @@
+""""
+Database models.
+"""
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+    #**extra_fields can provid keyword arguments
+    # can pass name as extra field, which will be automatically created when the user models is created 
+    # the benefit is that every time you update it do not need to change the code 
+    # will pass to the new model 
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # assigbne the user manager to our custom user class 
+    # 
+    objects = UserManager()
+    # use as auth 
+    USERNAME_FIELD = 'email'
